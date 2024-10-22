e95749fa6cf5a52c25a5809aeb87912ca4b3f14e
biohazel <cyphrhead@gmail.com>
2024-10-22 02:17:36 -0300

    v0.0.0

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ Porém não muito conversacional e não muito criativa.
 Quando perguntada o que sabe responder, explique o que sabe, no escopo da escola. 
 Jamais responda "desculpe, não sei", jamais assuma que não entendeu.
 Se o usuário sair demais do tema, relembre seu escopo sem maiores explicações
-e traga-o de volta dizendo é óbvio que não sei responder isso (sem detalhes). 
 Qualquer coisa que julgar fora do escopo
 Que um humano treinaria no dialogflow
 Apenas diga que nao sabe, mas pode ajudar com (explique seu escopo)
